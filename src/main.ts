1c9f12ba8a92bb75210476a143439c3ef67e6429
Igor Rybyakov <rybyakovigor@gmail.com>
2024-02-27 21:20:10 +0500

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // Core
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 // Module
 import { AppModule } from './app.module';
@@ -9,6 +10,9 @@ import { initializeLogger } from './modules/core/modules/logger/initialize';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, { bufferLogs: true, cors: true });
+
+  // Helmet
+  app.use(helmet());
 
   // Logger
   initializeLogger(app);
