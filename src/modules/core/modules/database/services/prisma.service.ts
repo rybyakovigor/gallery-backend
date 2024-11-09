5ad138ad1cb13c5e103852698284adadeb5ae195
Igor Rybyakov <rybyakovigor@gmail.com>
2024-11-10 02:12:21 +0500

    fix(migrations): rework add migrations

--- src/modules/core/modules/database/services/prisma.service.ts
+++ src/modules/core/modules/database/services/prisma.service.ts
@@ -1,10 +1,25 @@
 // Core
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
   public async onModuleInit(): Promise<void> {
-    await this.$connect();
+    try {
+      this.logger.log('Applying Prisma migrations...');
+      await execAsync('npx prisma migrate deploy');
+      this.logger.log('Migrations applied successfully.');
+
+      await this.$connect();
+      this.logger.log('Prisma connected to the database.');
+    } catch (error) {
+      this.logger.error('Error applying migrations or connecting to the database:', error);
+      throw error;
+    }
   }
 }
